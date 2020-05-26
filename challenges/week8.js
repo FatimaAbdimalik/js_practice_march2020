fb745d043d50b8f18fc5d69710f339b66576f168
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-05-26 10:29:59 +0100

    refractored some

--- challenges/week8.js
+++ challenges/week8.js
@@ -8,27 +8,20 @@ const findNextNumber = (nums, n) => {
 };
 
 const count1sand0s = (str) => {
-  // let str1 = str.split("");
-
-  // let count = str1.reduce((total, number) => {
-  //   total[number] = (total[number] || 0) + 1;
-  //   return total;
-  // }, {});
-  // console.log(count);
-  // return count;
-
   let obj = {
     "1": 0,
     "0": 0,
   };
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === "1") {
-      obj["1"]++;
-    } else {
-      obj["0"]++;
-    }
-  }
-  console.log(obj);
+  // for (let i = 0; i < str.length; i++) {
+  //   if (str[i] === "1") {
+  //     obj["1"]++;
+  //   } else {
+  //     obj["0"]++;
+  //   }
+  // }
+  // console.log(obj);
+  // return obj;
+  str.split("").forEach((num) => (num === "1" ? obj["1"]++ : obj["0"]++));
   return obj;
 };
 
